5d960e322612ff438836b5d302d8eb9c107ae30e
비니빈니 <115761494+pakyoong@users.noreply.github.com>
2023-03-24 15:30:47 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.01 ms, Memory: 4.43 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include<string.h>
+// 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
+int solution(const char* my_string) {
+    int answer = 0;
+    for(int i = 0; i < strlen(my_string); i++){
+        if(my_string[i] >= 48 && my_string[i] <=57){
+            answer += my_string[i] - 48;
+        }
+    }
+    return answer;
+}
