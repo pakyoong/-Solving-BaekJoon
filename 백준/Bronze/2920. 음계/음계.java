dbb04ec3c183291872fb3f93aed579c8089f7fe5
비니빈니 <phb4101@gmail.com>
2023-03-26 18:59:21 +0900

    [Bronze II] Title: 음계, Time: 204 ms, Memory: 17656 KB -BaekjoonHub

---
+++ 백준/Bronze/2920. 음계/음계.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Main {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		
+		int[] ascending = {1,2,3,4,5,6,7,8};
+		int[] descending = {8,7,6,5,4,3,2,1};
+		
+		int[] num = new int[8];
+		for(int i = 0; i < num.length; i++) {
+			num[i] = scan.nextInt();
+		}
+		
+		if(Arrays.equals(num, ascending)) {
+			System.out.println("ascending");
+		}
+		else if(Arrays.equals(num, descending)){
+			System.out.println("descending");
+		}
+		else {
+			System.out.println("mixed");
+		}
+		scan.close();
+	}
+
+}
