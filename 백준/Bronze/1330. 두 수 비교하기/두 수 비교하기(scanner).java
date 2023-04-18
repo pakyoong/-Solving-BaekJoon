9237c6e985c6564142bfa4f978a947383c5f1886
비니빈니 <phb4101@gmail.com>
2023-04-19 08:54:00 +0900

    Update 두 수 비교하기(scanner).java

--- 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기(scanner).java
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기(scanner).java
@@ -1,22 +1,19 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        
-        int A = Integer.parseInt(st.nextToken());
-        int B = Integer.parseInt(st.nextToken());
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+        scanner.close();
 
         if (A > B) {
             System.out.println(">");
-        } else if (A < B){
+        } else if (A < B) {
             System.out.println("<");
         } else {
             System.out.println("==");
         }
+
     }
 }
